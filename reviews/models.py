0bfe520d3e4a23460b057032f2044e98088c2ff2
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-04-18 03:08:12 +0900

    python query set and finished admin

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Review(core_models.TimeStampedModel):
+    """ Review Model Definition """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanLiness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+
+    user = models.ForeignKey(
+        "users.User", related_name="reviews", on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reviews", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.review} - {self.room}"
--- rooms/admin.py
+++ rooms/admin.py
@@ -6,6 +6,11 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     """ Item Admin Definition """
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -13,7 +18,79 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """ Item Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        (
+            "Spaces",
+            {
+                "fields": (
+                    "guests",
+                    "beds",
+                    "bedrooms",
+                    "baths",
+                )
+            },
+        ),
+        (
+            "More About Space",
+            {
+                "fields": (
+                    "amenities",
+                    "facilities",
+                    "house_roles",
+                )
+            },
+        ),
+        ("Last Detail", {"fields": ("host",)}),
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photo",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_roles",
+        "city",
+        "country",
+    )
+
+    search_fields = ("city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_roles",
+    )
+
+    # obj => current row data
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photo(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
